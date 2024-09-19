b05d306d4bc3073251b081be08a8058b64d38e74
DuskSunShine <437433575@qq.com>
2024-09-19 13:46:06 +0800

    call

--- contracts/solidity/solidity102/callContract/CallContract.sol
+++ contracts/solidity/solidity102/callContract/CallContract.sol
@@ -1,29 +1,70 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.22 <0.9.0;
 import "contracts/solidity/solidity102/callContract/OtherContract.sol";
-contract CallContract{
 
-    function callOtherSetX(address other,uint256 x)external {
+contract CallContract {
+    function callOtherSetX(address payable other, uint256 x) external {
         OtherContract(other).setX(x);
     }
 
-    function interfaceCallOtherSetX(address other,uint256 x)external {
+    function interfaceCallOtherSetX(address other, uint256 x) external {
         Icontract(other).setX(x);
     }
 
-    function callOtherGetX(OtherContract other) external view returns (uint256){
+    function callOtherGetX(OtherContract other)
+        external
+        view
+        returns (uint256)
+    {
         return other.getX();
     }
-    function callGetX2(address other) external view returns(uint x){
+
+    function callGetX2(address payable other)
+        external
+        view
+        returns (uint256 x)
+    {
         OtherContract oc = OtherContract(other);
         x = oc.getX();
     }
 
-     function setXTransferETH(address otherContract, uint256 x) payable external{
+    function setXTransferETH(address payable otherContract, uint256 x)
+        external
+        payable
+    {
         OtherContract(otherContract).setX{value: x}(x);
     }
 
+    function callSetX(address payable _addr, uint256 x)
+        public
+        payable
+        returns (uint256)
+    {
+        // call setX()，同时可以发送ETH data目标函数返回值
+        (bool success, bytes memory data) = _addr.call{value: msg.value}(
+            abi.encodeWithSignature("setX(uint256)", x)
+        );
+        if (success) {
+            // 解析返回值
+            return abi.decode(data, (uint256));
+        } else {
+            return 0;
+        }
+    }
 
+    function callGetX(address _addr) external returns (uint256) {
+        (bool success, bytes memory data) = _addr.call(
+            abi.encodeWithSignature("getX()")
+        );
+        if (success) {
+            // 解析返回值
+            return abi.decode(data, (uint256));
+        } else {
+            return 0;
+        }
+    }
 
-    receive() external payable { }
+    receive() external payable {}
+
+    fallback() external payable {}
 }
--- contracts/solidity/solidity102/callContract/OtherContract.sol
+++ contracts/solidity/solidity102/callContract/OtherContract.sol
@@ -3,16 +3,17 @@ pragma solidity >=0.8.22 <0.9.0;
 
 interface Icontract {
     function getBalance() external view returns (uint256);
-    function setX(uint256 x)external payable;
-    function getX()external view returns (uint256);
+
+    function setX(uint256 x) external payable;
+
+    function getX() external view returns (uint256);
 }
 
-abstract contract AbsOtherContract{
-    function absCallOtherGetX() virtual external view returns (uint256);
+abstract contract AbsOtherContract {
+    function absCallOtherGetX() external view virtual returns (uint256);
 }
 
-
-contract OtherContract is Icontract,AbsOtherContract {
+contract OtherContract is Icontract, AbsOtherContract {
     uint256 private _x = 0;
     // 收到eth的事件，记录amount和gas
     event Log(uint256 amount, uint256 gas);
@@ -21,18 +22,44 @@ contract OtherContract is Icontract,AbsOtherContract {
         return address(this).balance;
     }
 
-     function absCallOtherGetX() override external view returns (uint256){
-            return _x;
+    function absCallOtherGetX() external view override returns (uint256) {
+        return _x;
     }
 
-    function setX(uint256 x)external payable {
-        _x=x;
-        if (msg.value>0){
-            emit Log(msg.value,gasleft());
+    function setX(uint256 x) external payable {
+        _x = x;
+        if (msg.value > 0) {
+            emit Log(msg.value, gasleft());
         }
     }
 
-    function getX() external view returns (uint256){
+    function getX() external view returns (uint256) {
         return _x;
+    }
+
+    function callSetX(address payable _addr, uint256 x) public payable {
+        // call setX()，同时可以发送ETH data目标函数返回值
+        (bool success, bytes memory data) = _addr.call{value: msg.value}(
+            abi.encodeWithSignature("setX(uint256)", x)
+        );
+        if (success) {
+            emit Response(success, data);
+        }
+    }
+
+    // 定义Response事件，输出call返回的结果success和data
+    event Response(bool success, bytes data);
+
+    fallback() external payable {}
+
+    receive() external payable {}
+
+    function callNonExist(address _addr) external {
+        // call 不存在的函数
+        (bool success, bytes memory data) = _addr.call(
+            abi.encodeWithSignature("foo(uint256)")
+        );
+
+        emit Response(success, data); //释放事件
     }
 }
